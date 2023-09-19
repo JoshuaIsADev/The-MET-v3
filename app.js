a875763a1ab87223e974117cc146c7bdbba7b185
JoshuaIsADev <JoshuaIsADev@gmail.com>
2023-09-19 15:41:16 -0700

    added event listener to js, and css adjust

--- app.js
+++ app.js
@@ -2,12 +2,54 @@ const btnMin = document.querySelector('.btn-minimize');
 const artDisplay = document.querySelector('.art-display');
 const bodyDocument = document.body;
 
-btnMin.addEventListener('click', function () {
+function scaleImage() {
   artDisplay.classList.toggle('min');
   btnMin.classList.toggle('rotate180');
   bodyDocument.classList.toggle('overflow');
+}
+
+//SCALE IMAGE EVENT LISTENER
+btnMin.addEventListener('click', function () {
+  scaleImage();
 });
 
-//minimize image
+bodyDocument.addEventListener('keydown', function (e) {
+  if (e.key === ' ') {
+    scaleImage();
+  }
+});
 
-// bodyDocument.style.overflowY = 'auto';
+//NEXT IMAGE EVENT LISTENER
+bodyDocument.addEventListener('keydown', function (e) {
+  if (e.key === 'ArrowRight') {
+    console.log('next image');
+  }
+});
+
+// FETCH ARTWORK
+// const fetchArtwork = async () => {
+//   const responseArtwork = await axios.get(
+//     'https://collectionapi.metmuseum.org/public/collection/v1/search',
+//     {
+//       params: {
+//         q: '',
+//         medium: 'Paintings',
+//         hasImages: 'true',
+//         isOnView: 'true',
+//       },
+//     }
+//   );
+// };
+
+// axios
+//   .get('https://collectionapi.metmuseum.org/public/collection/v1/search', {
+//     params: {
+//       q: '',
+//       medium: 'Paintings',
+//       hasImages: 'true',
+//       isOnView: 'true',
+//     },
+//   })
+//   .then((res) => {
+//     console.log(res);
+//   });
